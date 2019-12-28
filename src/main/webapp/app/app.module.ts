d5d4a983b39ca37f9aadbddcb5a48a861feaa19f
simonL <sledrapier@gmail.com>
2019-12-28 22:03:14 +0100

    [1.0] modification interface

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -6,8 +6,6 @@ import { CartoSharedModule } from 'app/shared/shared.module';
 import { CartoCoreModule } from 'app/core/core.module';
 import { CartoAppRoutingModule } from './app-routing.module';
 import { CartoHomeModule } from './home/home.module';
-import { CartoEntityModule } from './entities/entity.module';
-// jhipster-needle-angular-add-module-import JHipster will add new module here
 import { JhiMainComponent } from './layouts/main/main.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
@@ -16,15 +14,7 @@ import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CartoSharedModule,
-    CartoCoreModule,
-    CartoHomeModule,
-    // jhipster-needle-angular-add-module JHipster will add new module here
-    CartoEntityModule,
-    CartoAppRoutingModule
-  ],
+  imports: [BrowserModule, CartoSharedModule, CartoCoreModule, CartoHomeModule, CartoAppRoutingModule],
   declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
   bootstrap: [JhiMainComponent]
 })
